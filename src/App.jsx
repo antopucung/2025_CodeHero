c5a4fdb4ead83f151971410d75105ed317baef3c
antopucung <76433576+antopucung@users.noreply.github.com>
2025-07-14 20:47:15 +0700

    Fix Header Import and Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Box } from "@chakra-ui/react";
-import { motion } from "framer-motion";
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import MarketplacePage from './pages/MarketplacePage';
-import CommunityPage from './pages/CommunityPage';
-import CommissionDetailPage from './pages/CommissionDetailPage';
-import ModuleDetailPage from './pages/ModuleDetailPage';
-import LessonPage from './pages/LessonPage';
-
-const MotionBox = motion(Box);
-
-function App() {
-  return (
-    <Router>
-      <Box 
-        w="100vw"
-        h="100vh"
-        bg="#000000" 
-        color="#00ff00" 
-        fontFamily="'Courier New', monospace"
-        overflow="hidden"
-        display="flex"
-        flexDirection="column"
-        position="fixed"
-        top="0"
-        left="0"
-      >
-        {/* Global Header - Fixed Height */}
-        <Box flexShrink={0} h="60px" overflow="hidden">
-          <Header />
-        </Box>
-        
-        {/* Main Content - Dynamic Height */}
-        <MotionBox
-          flex={1}
-          overflow="hidden"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.3 }}
-          display="flex"
-          flexDirection="column"
-        >
-          <Routes>
-            <Route path="/" element={<HomePage />} />
             <Route path="/marketplace" element={<MarketplacePage />} />
             <Route path="/community" element={<CommunityPage />} />
             <Route path="/commission/:id" element={<CommissionDetailPage />} />
@@ -57,7 +10,16 @@ function App() {
         </MotionBox>
       </Box>
     </Router>
-  );
-}
-
-export default App;
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          initial={{ opacity: 0 }}
+          display="flex"
+          overflow="hidden"
+          flexDirection="column"
+          flex={1}
+        >
+        <MotionBox
+          <Routes>
+        {/* Main Content - Dynamic Height */}
+            <Route path="/" element={<HomePage />} />
+        

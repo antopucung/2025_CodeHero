bf4dd3fd675e9fd194a73c7d543248cd4b0af130
antopucung <76433576+antopucung@users.noreply.github.com>
2025-07-15 17:45:39 +0700

    Updated DraggableCodeBlock.jsx

--- src/components/learning/DraggableCodeBlock.jsx
+++ src/components/learning/DraggableCodeBlock.jsx
@@ -4,8 +4,11 @@
        drag={!isPlaced}
        dragSnapToOrigin
        onDragStart={() => onDragStart(block)}
+   )
 -      onDragEnd={() => onDragEnd(block)}
 +      onDragEnd={(event, info) => onDragEnd(block, info)}
        whileHover={{ scale: isPlaced ? 1 : 1.02 }}
        whileDrag={{ scale: 1.05, zIndex: 10 }}
        animate={{
+       }
+       }

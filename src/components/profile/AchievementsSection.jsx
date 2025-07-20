117cc51e14d1312f533548bcf4e8641f37ed6b24
antopucung <76433576+antopucung@users.noreply.github.com>
2025-07-20 23:43:34 +0700

    Updated AchievementsSection.jsx

--- src/components/profile/AchievementsSection.jsx
+++ src/components/profile/AchievementsSection.jsx
@@ -33,7 +33,7 @@ export function AchievementsSection({ achievements }) {
                 }}
                 animated
               />
-          ))
+            ))}
           </Grid>
         ) : (
           <Stack>
